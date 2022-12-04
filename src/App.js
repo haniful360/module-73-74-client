c21b53324d5e414c10891f4c7932bd6ff8303d79
Haniful Islam <haniful360@gmail.com>
2022-12-05 03:14:54 +0600

    singup section added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Apointment from './Pages/Apointment/Apointment';
 import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
+import Login from './Pages/Login/Login'; 
+import SingUp from './Pages/Login/SignUp';
+// import SignUp from './Pages/SignUp/SignUp'; 
 import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 
@@ -16,6 +18,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/appiontment" element={<Apointment />} />
         <Route path="/login" element={<Login/>} />
+        <Route path = '/signup' element={<SingUp></SingUp>}></Route>
       </Routes>
       <Footer></Footer>
     </div>

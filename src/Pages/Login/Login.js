532c7615410a70044162cf42210145fc3796e718
Haniful Islam <haniful360@gmail.com>
2022-12-05 02:05:18 +0600

    email and password validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,25 +1,92 @@
 import React from 'react';
 import auth from '../../firebase.init';
-import { useSignInWithGoogle, useSignInWithGithub } from 'react-firebase-hooks/auth';
+import { useSignInWithGoogle, useSignInWithGithub, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    const [signInWithGithub, user1, loading1, error2] = useSignInWithGithub(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+    const [signInWithGithub, githubuser, githubloading, githubError] = useSignInWithGithub(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    if(user || gUser || githubuser){
+        console.log(user || gUser || githubuser);
+    }
+    if (gLoading || githubloading || loading) {
+        return <Loading></Loading>
+    }
+    let signInError;
+    if (error || gError || githubError) {
+        signInError = <small><p className='text-red-500 mb-2'>{error?.message || gError || githubError}</p></small>
+    }
+    const onSubmit = data => {
+        signInWithEmailAndPassword(data.email, data.password);
+    }
     return (
+
         <div className='flex h-screen justify-center items-center '>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center font-bold">Login</h2>
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label">
-                            <span className="label-text">What is your name?</span>
-                        </label>
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        <label className="label">
-                            <span className="label-text-alt">Alt label</span> 
-                        </label>
-                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Email address"
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is required'
+                                    },
+                                    pattern: {
+                                        value: /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'Provide a valid email address'
+                                    }
 
+                                })}
+                            />
+                            <label className="label">
+                                {errors.email?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.email?.message}</span>}
+                                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email?.message}</span>}
+                            </label>
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="password"
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'password is required'
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: 'Must be at least 6 characters'
+                                    }
+
+                                })}
+                            />
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                            </label>
+                        </div>
+                        {signInError}
+                        <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+                    </form>
                     <div className="divider">OR</div>
                     <button onClick={() => signInWithGoogle()}
                         className="btn btn-outline">Continue with Google</button>
---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+    return (
+        <div className='flex h-screen justify-center items-center'>
+            <button className="btn loading"></button>
+        </div>
+      
+    );
+};
+
+export default Loading;
